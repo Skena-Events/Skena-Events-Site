9012021698b1eb5598fcf0912b1e7eeeac5c0dc8
Skena-Events <skenaeventslv@gmail.com>
2025-09-13 17:08:28 +0300

    Update kontakt.tsx

--- src/pages/kontakt.tsx
+++ src/pages/kontakt.tsx
@@ -1,1 +1,8 @@
-// Kontakt page code
+export default function KontaktPage() {
+  return (
+    <section>
+      <h1>Kontakti</h1>
+      <p>E-pasts: info@skenaevents.lv • Tālr.: +371 ____</p>
+    </section>
+  );
+}
