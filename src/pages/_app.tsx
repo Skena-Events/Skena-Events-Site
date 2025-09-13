cecf27de178fb78d9b4f885b501e4b01c19173a4
Skena-Events <skenaeventslv@gmail.com>
2025-09-13 15:08:08 +0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
+// src/pages/_app.tsx
 import type { AppProps } from 'next/app';
+import Layout from '../components/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
