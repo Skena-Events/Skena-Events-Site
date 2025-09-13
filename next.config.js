ff740316467cfb95e937781bdcf314f98b28abaf
Skena-Events <skenaeventslv@gmail.com>
2025-09-13 15:05:20 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,6 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // если проект в /src — ничего доп. делать не нужно, Next это поддерживает
+};
+module.exports = nextConfig;
