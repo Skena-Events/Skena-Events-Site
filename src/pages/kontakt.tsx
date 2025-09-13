56789002efed43380dbe19d22ea98deae3250718
Skena-Events <skenaeventslv@gmail.com>
2025-09-13 17:22:43 +0300

    Update kontakt.tsx

--- src/pages/kontakt.tsx
+++ src/pages/kontakt.tsx
@@ -1,8 +1,12 @@
+import Navbar from "@/components/Navbar";
 export default function KontaktPage() {
   return (
-    <section>
-      <h1>Kontakti</h1>
-      <p>E-pasts: info@skenaevents.lv • Tālr.: +371 ____</p>
-    </section>
+    <>
+      <Navbar />
+      <main className="container py-10">
+        <h1 className="text-2xl font-bold mb-2">Kontakti</h1>
+        <p className="text-neutral-300">E-pasts: info@skenaevents.lv • Tālr.: +371 ______</p>
+      </main>
+    </>
   );
 }
