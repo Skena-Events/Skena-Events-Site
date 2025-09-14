23ed081c103d1a5ac6d79b71ba8afafa48eeb50f
Skena-Events <skenaeventslv@gmail.com>
2025-09-15 00:01:40 +0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter, JetBrains_Mono } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"], display: "swap" });
+const mono = JetBrains_Mono({ subsets: ["latin"], display: "swap", variable: "--font-mono" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${inter.className} ${mono.variable}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
